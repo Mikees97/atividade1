7d8a36dd499c21255e848d41637e15c83ba6a716
Michael Alexandre <157851100+Mikees97@users.noreply.github.com>
2024-02-07 20:55:22 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,119 @@
+//Atividade 1 
+//let dia = true
+//if (dia){
+//    console.log("Claro");
+//}
+//else{
+//    console.log("De noite");
+//}
+
+
+//Atividade 2
+//for (let n = 0; n <=20; n+=2) {
+//    console.log (n)
+//    }
+
+
+
+//Atividade 3
+//function message(){
+//   let dizerOi ="oi"; 
+//    console.log("oi");
+//}
+//    message()
+
+
+
+//Atividade 4
+//function meuNome(michael) {
+//    console.log(michael);
+//    }
+//    meuNome("Michael")
+
+
+
+//Atividade 5
+//function mostrarDados(nome, idade, estiloMusical) {
+//    console.log(nome, idade, estiloMusical);
+//    }
+//    mostrarDados ("Michael", "26", "Rap");
+
+
+
+//Atividade 6
+//function mostrarDados(filme, musica) {
+//    console.log(filme, musica);
+//    }
+//    mostrarDados ("Clube da Luta", "Teto - Mais um voo");
+
+
+//Atividade 7
+//function triple(numero){
+//    return numero*3
+//    }
+//    console.log(triple(7))
+    
+
+
+//Atividade 8
+//function verifica(){
+//    let verdadeiroOuFalso = true;
+//    if(verdadeiroOuFalso){
+//        console.log("verdadeiro")
+//    } else{
+//        console.log("falso")
+//    }
+//}
+//verifica();
+
+
+
+//Atividade 9
+//let nomes = ["Andre", "Lucas", "Guilherme", "Michael", "Vitor"];
+//console.log(nomes);
+
+
+
+//Atividade 10
+//let nomes = ["Andre", "Lucas", "Guilherme", "Michael", "Vitor"];
+//nomes.unshift("Gabriel");
+//console.log(nomes);
+
+
+
+//Atividade 11
+//let nomes = ["Andre", "Lucas", "Guilherme", "Michael", "Vitor"];
+//nomes.pop();
+//console.log(nomes);
+
+
+
+//Atividade 12
+//let nomes = ["Andre", "Lucas", "Guilherme", "Michael", "Vitor"];
+//nomes.push("Nathan", "Nicolas");
+//console.log(nomes);
+
+
+
+//Atividade 13
+//let nomes = ["Andre", "Lucas", "Guilherme", "Michael", "Vitor"];
+//nomes.shift();
+//console.log(nomes);
+
+
+
+//Atividade 14
+//const nomes = ["Andre", "Lucas", "Guilherme", "Michael", "Vitor"];
+//nomes.splice(3, 0,'Joao');
+//nomes.splice(4, 0,'Maria');
+//nomes.splice(5, 0,'Jose');
+//nomes.splice(6, 0,'Pedro');
+//console.log(nomes);
+
+//Atividade 15
+//let numeros = [7,5,6,3,8,9,2,1,4]
+//numeros.sort( 
+//function (a, b) {
+//return (a-b);
+//});
+//console.log(numeros)
